31ceff77f796471f05994dff054bb0dccde41c5a
AjayDiaz76 <ajaydiaz@gmail.com>
2024-09-26 23:16:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BMI_Calculator from './BMI_Calculator/BMI_Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <BMI_Calculator />
+    </div>
+  );
+}
+
+export default App;
